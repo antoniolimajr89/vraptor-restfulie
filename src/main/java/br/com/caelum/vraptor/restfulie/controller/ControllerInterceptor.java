2b0be5fc1710dc8665078adf4864f2c1b53c8cbc
Felipe Oliveira <felipeweb.programador@gmail.com>
2014-12-16 17:09:46 -0200

    Mudando scope

--- src/main/java/br/com/caelum/vraptor/restfulie/controller/ControllerInterceptor.java
+++ src/main/java/br/com/caelum/vraptor/restfulie/controller/ControllerInterceptor.java
@@ -1,15 +1,5 @@
 package br.com.caelum.vraptor.restfulie.controller;
 
-import java.lang.reflect.Method;
-import java.lang.reflect.ParameterizedType;
-import java.lang.reflect.Type;
-import java.util.Arrays;
-import java.util.EnumSet;
-import java.util.List;
-
-import javax.enterprise.context.RequestScoped;
-import javax.inject.Inject;
-
 import br.com.caelum.vraptor.InterceptionException;
 import br.com.caelum.vraptor.Intercepts;
 import br.com.caelum.vraptor.controller.ControllerMethod;
@@ -25,97 +15,107 @@ import br.com.caelum.vraptor.restfulie.relation.Relation;
 import br.com.caelum.vraptor.restfulie.relation.RelationBuilder;
 import br.com.caelum.vraptor.view.Status;
 
+import javax.enterprise.context.Dependent;
+import javax.inject.Inject;
+import javax.servlet.http.HttpServletRequest;
+import java.lang.reflect.Method;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.Arrays;
+import java.util.EnumSet;
+import java.util.List;
+
 @Intercepts
-@RequestScoped
+@Dependent
 public class ControllerInterceptor<T extends HypermediaController>
-		implements Interceptor {
+        implements Interceptor {
 
-	private final ControllerControl<T> control;
-	private final List<Class<?>> controllers;
-	private final Status status;
-	private final Restfulie restfulie;
-	private final Route routes;
-	private final RequestStarted info;
-	private final ParameterizedTypeSearcher searcher = new ParameterizedTypeSearcher();
+    private final ControllerControl<T> control;
+    private final List<Class<?>> controllers;
+    private final Status status;
+    private final Restfulie restfulie;
+    private final Route routes;
+    private final HttpServletRequest request;
+    private final ParameterizedTypeSearcher searcher = new ParameterizedTypeSearcher();
 
-	/**
-	 * @deprecated CDI eyes only
-	 */
-	protected ControllerInterceptor() {
-		this(null, null, null, null, null);
-	}
+    /**
+     * @deprecated CDI eyes only
+     */
+    protected ControllerInterceptor() {
+        this(null, null, null, null, null);
+    }
 
-	@Inject
-	public ControllerInterceptor(ControllerControl<T> control,
-			Restfulie restfulie, Status status, RequestStarted info,
-			Route routes) {
-		this.control = control;
-		this.restfulie = restfulie;
-		this.status = status;
-		this.info = info;
-		this.routes = routes;
-		this.controllers = Arrays.asList(control.getControllers());
-	}
+    @Inject
+    public ControllerInterceptor(ControllerControl<T> control,
+                                 Restfulie restfulie, Status status, HttpServletRequest request,
+                                 Route routes) {
+        this.control = control;
+        this.restfulie = restfulie;
+        this.status = status;
+        this.request = request;
+        this.routes = routes;
+        this.controllers = Arrays.asList(control.getControllers());
+    }
 
-	public boolean accepts(ControllerMethod method) {
-		return controllers.contains(method.getController().getType())
-				&& method.getMethod().isAnnotationPresent(Transition.class);
-	}
+    public boolean accepts(ControllerMethod method) {
+        return controllers.contains(method.getController().getType())
+                && method.getMethod().isAnnotationPresent(Transition.class);
+    }
 
-	public void intercept(InterceptorStack stack, ControllerMethod method,
-			Object instance) throws InterceptionException {
-		ParameterizedType type = searcher.search(control.getClass());
-		if (analyzeImplementation(method, type)) {
-			stack.next(method, instance);
-		}
-	}
+    public void intercept(InterceptorStack stack, ControllerMethod method,
+                          Object instance) throws InterceptionException {
+        ParameterizedType type = searcher.search(control.getClass());
+        if (analyzeImplementation(method, type)) {
+            stack.next(method, instance);
+        }
+    }
 
-	private boolean analyzeImplementation(ControllerMethod method,
-			ParameterizedType parameterized) {
-		Type parameterType = parameterized.getActualTypeArguments()[0];
-		Class<?> found = (Class<?>) parameterType;
-		T controller = retrieveResource(found);
-		if (controller == null) {
-			status.notFound();
-			return false;
-		}
-		if (allows(controller, method.getMethod())) {
-			return true;
-		}
-		status.methodNotAllowed(allowedMethods());
-		return false;
-	}
+    private boolean analyzeImplementation(ControllerMethod method,
+                                          ParameterizedType parameterized) {
+        Type parameterType = parameterized.getActualTypeArguments()[0];
+        Class<?> found = (Class<?>) parameterType;
+        T controller = retrieveResource(found);
+        if (controller == null) {
+            status.notFound();
+            return false;
+        }
+        if (allows(controller, method.getMethod())) {
+            return true;
+        }
+        status.methodNotAllowed(allowedMethods());
+        return false;
+    }
 
-	private EnumSet<HttpMethod> allowedMethods() {
-		EnumSet<HttpMethod> allowed = routes.allowedMethods();
-		allowed.remove(HttpMethod.of(info.getRequest()));
-		return allowed;
-	}
+    private EnumSet<HttpMethod> allowedMethods() {
+        EnumSet<HttpMethod> allowed = routes.allowedMethods();
+        allowed.remove(HttpMethod.of(request));
+        return allowed;
+    }
 
-	private T retrieveResource(Class<?> found) {
-		String parameterName = lowerFirstChar(found.getSimpleName()) + ".id";
-		String id = info.getRequest().getParameter(parameterName);
-		T controller = control.retrieve(id);
-		return controller;
-	}
+    private T retrieveResource(Class<?> found) {
+        String parameterName = lowerFirstChar(found.getSimpleName()) + ".id";
+        String id = request.getParameter(parameterName);
+        T controller = control.retrieve(id);
+        return controller;
+    }
 
-	private boolean allows(T controller, Method method) {
-		RelationBuilder builder = restfulie.newRelationBuilder();
-		controller.configureRelations(builder);
+    private boolean allows(T controller, Method method) {
+        RelationBuilder builder = restfulie.newRelationBuilder();
+        controller.configureRelations(builder);
 
-		for (Relation relation : builder.getRelations()) {
-			if (relation.matches(method)) {
-				return true;
-			}
-		}
-		return false;
-	}
+        for (Relation relation : builder.getRelations()) {
+            if (relation.matches(method)) {
+                return true;
+            }
+        }
+        return false;
+    }
 
-	private String lowerFirstChar(String simpleName) {
-		if (simpleName.length() == 1) {
-			return simpleName.toLowerCase();
-		}
-		return Character.toLowerCase(simpleName.charAt(0))
-				+ simpleName.substring(1);
-	}
+    private String lowerFirstChar(String simpleName) {
+        if (simpleName.length() == 1) {
+            return simpleName.toLowerCase();
+        }
+        return Character.toLowerCase(simpleName.charAt(0))
+                + simpleName.substring(1);
+    }
 }
